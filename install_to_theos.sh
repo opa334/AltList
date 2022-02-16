4832a0a63bca0227e6a6b5e761c18c1ac77da28c
UsrLightmann <lightmanns11@gmail.com>
2022-02-16 12:50:33 -0500

    Install headers with install script

--- install_to_theos.sh
+++ install_to_theos.sh
@@ -2,4 +2,8 @@ set -e
 make clean
 make FINALPACKAGE=1
 cp -Rv "./.theos/obj/AltList.framework" "$THEOS/lib"
+
+mkdir -p "$THEOS/include/AltList"
+find . -maxdepth 1 -name "ATL*.h" -exec cp -v {} "$THEOS/include/AltList" \; 
+cp -v AltList.h "$THEOS/include/AltList"
 echo "Successfully installed AltList"
